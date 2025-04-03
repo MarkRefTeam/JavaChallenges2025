907a8c56f08dad316b452c9585afc6ecc3f64787
MarkRefTeam <szllsy.mark@gmail.com>
2025-04-03 18:13:59 +0200

    Add PlaneFigures – Project for shape drawing

---
+++ PlaneFigures/src/PlaneFigures.java
@@ -0,0 +1,66 @@
+public class PlaneFigures {
+
+    public static void main(String[] args) {
+
+        System.out.println("Négyzet");
+        System.out.println();
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+
+        System.out.println("Téglalap");
+        System.out.println();
+        System.out.println("************************");
+        System.out.println("************************");
+        System.out.println("************************");
+        System.out.println("************************");
+        System.out.println("************************");
+
+        System.out.println("Paralelogramma");
+        System.out.println();
+        System.out.println("         ********");
+        System.out.println("        ********");
+        System.out.println("       ********");
+        System.out.println("      ********");
+
+        System.out.println("trapéz");
+        System.out.println();
+        System.out.println("    ********   ");
+        System.out.println("   **********   ");
+        System.out.println("  ************   ");
+        System.out.println(" **************   ");
+
+        System.out.println("rombusz");
+        System.out.println();
+        System.out.println("     *     ");
+        System.out.println("    ***     ");
+        System.out.println("   *****     ");
+        System.out.println("    ***     ");
+        System.out.println("     *     ");
+
+        System.out.println("Deltoid");
+        System.out.println();
+        System.out.println("     *     ");
+        System.out.println("    ***     ");
+        System.out.println("   *****     ");
+        System.out.println("  *******     ");
+        System.out.println("   *****     ");
+        System.out.println("    ***     ");
+
+        System.out.println("Háromszög");
+        System.out.println();
+        System.out.println("     *     ");
+        System.out.println("    ***     ");
+        System.out.println("   *****     ");
+        System.out.println("  *******     ");
+
+
+
+
+    }
+
+
+
+
+    }
