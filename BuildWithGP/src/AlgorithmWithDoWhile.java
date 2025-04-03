be1e4f9b2fa7e9d0894e00bc91585becc44f5562
MarkRefTeam <szllsy.mark@gmail.com>
2025-04-03 18:28:17 +0200

    Add BuildWithGP – Includes first working calculator and control flow examples

---
+++ BuildWithGP/src/AlgorithmWithDoWhile.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class AlgorithmWithDoWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter number between 1 and 10");
+        int number = sc.nextInt();
+        int wrongInputCounter = 0;
+
+        do {
+            if (number < 1 || number > 10) {
+                System.out.println("Enter number between 1 and 10");
+                wrongInputCounter++;
+
+                if (wrongInputCounter > 5) {
+                    System.out.println("Invalid choice! Too many wrong attempts. Exiting...");
+                    return;
+                }
+
+                number = sc.nextInt();
+            }
+        } while (number < 1 || number > 10);
+
+        System.out.println("Valid input received: " + number);
+
+        sc.close();
+    }
+
+
+}
+
+
+
+
+
