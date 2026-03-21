39ad989ad1750293e79fb511790c7fd495e9b720
Mark Szollosy <szllsy.mark@gmail.com>
2026-03-21 11:19:34 +0100

    Add springdemo-469 Spring Boot project

---
+++ springdemo-469/src/main/java/com/mark/springdemo/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.mark.springdemo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse handleValidationException(MethodArgumentNotValidException ex) {
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .toList();
+
+        return new ValidationErrorResponse("Validation failed", errors);
+    }
+}
